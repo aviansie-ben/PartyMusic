d8529be406a54ddacdd0930b93ac7caa89cb0d9f
Ben Thomas <bendude56@gmail.com>
2011-12-10 17:24:47 -0700

    -Added a cool little header thing -Made it so that if BenCmd is not active, it will default to ops only -Added /music list command to list songs

--- src/com/bendude56/partymusic/PartyMusic.java
+++ src/com/bendude56/partymusic/PartyMusic.java
@@ -19,6 +19,8 @@ import com.bendude56.bencmd.User;
 
 public class PartyMusic extends JavaPlugin {
 	
+	public static final String HEADER = ChatColor.GREEN + "~--~" + ChatColor.YELLOW + " PartyMusic " + ChatColor.GREEN + "~--~";
+	
 	public Logger log = Logger.getLogger("Minecraft");
 	public Properties list;
 
@@ -44,30 +46,38 @@ public class PartyMusic extends JavaPlugin {
 			log.log(Level.SEVERE, "[PartyMusic] Something broke!", e);
 		}
 	}
+	
+	public boolean BCE() {
+		return Bukkit.getServer().getPluginManager().isPluginEnabled("BenCmd");
+	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command,
 			String label, String[] args) {
 		User user = User.getUser(sender);
 		if (args.length == 0) {
+			sender.sendMessage(HEADER);
 			sender.sendMessage(ChatColor.RED + "Proper use is: /music {play|stop|set|remove}");
 		} else if (args[0].equalsIgnoreCase("play")) {
-			if (!user.hasPerm("pmusic.play")) {
+			if (!((BCE() && user.hasPerm("pmusic.play")) || sender.isOp())) {
 				sender.sendMessage(ChatColor.RED + "You don't have permission to do that!");
 			} else if (args.length != 2) {
 				sender.sendMessage(ChatColor.RED + "Proper use is: /music play <name>");
 			} else {
-				log.info("[PartyMusic] Playing " + args[1] + "...");
 				if (list.containsKey(args[1])) {
+					log.info("[PartyMusic] Playing " + args[1] + "...");
 					for (Player p : Bukkit.getOnlinePlayers()) {
 						SpoutManager.getSoundManager().playCustomMusic(this, SpoutManager.getPlayer(p), list.getProperty(args[1]), true);
 					}
+					sender.sendMessage(HEADER);
+					sender.sendMessage(ChatColor.GREEN + "Now playing " + args[1] + "!");
 				} else {
+					sender.sendMessage(HEADER);
 					sender.sendMessage(ChatColor.RED + "Music not found! Use /music set <name> <url> first!");
 				}
 			}
 		} else if (args[0].equalsIgnoreCase("stop")) {
-			if (!user.hasPerm("pmusic.stop")) {
+			if (!((BCE() && user.hasPerm("pmusic.stop")) || sender.isOp())) {
 				sender.sendMessage(ChatColor.RED + "You don't have permission to do that!");
 			} else if (args.length != 1) {
 				sender.sendMessage(ChatColor.RED + "Proper use is: /music stop");
@@ -76,9 +86,11 @@ public class PartyMusic extends JavaPlugin {
 				for (Player p : Bukkit.getOnlinePlayers()) {
 					SpoutManager.getSoundManager().stopMusic(SpoutManager.getPlayer(p));
 				}
+				sender.sendMessage(HEADER);
+				sender.sendMessage(ChatColor.GREEN + "All music is now stopped!");
 			}
 		} else if (args[0].equalsIgnoreCase("set")) {
-			if (!user.hasPerm("pmusic.set")) {
+			if (!((BCE() && user.hasPerm("pmusic.set")) || sender.isOp())) {
 				sender.sendMessage(ChatColor.RED + "You don't have permission to do that!");
 			} else if (args.length != 3) {
 				sender.sendMessage(ChatColor.RED + "Proper use is: /music set <name> <url>");
@@ -90,10 +102,11 @@ public class PartyMusic extends JavaPlugin {
 				} catch (Exception e) {
 					log.log(Level.SEVERE, "[PartyMusic] Failed to save music database:", e);
 				}
-				sender.sendMessage(ChatColor.GREEN + args[1] + " has successfully been set to " + args[2] + "!");
+				sender.sendMessage(HEADER);
+				sender.sendMessage(ChatColor.GREEN + args[1] + " has been successfully set to " + args[2] + "!");
 			}
 		} else if (args[0].equalsIgnoreCase("remove")) {
-			if (!user.hasPerm("pmusic.set")) {
+			if (!((BCE() && user.hasPerm("pmusic.set")) || sender.isOp())) {
 				sender.sendMessage(ChatColor.RED + "You don't have permission to do that!");
 			} else if (args.length != 2) {
 				sender.sendMessage(ChatColor.RED + "Proper use is: /music remove <name>");
@@ -105,10 +118,46 @@ public class PartyMusic extends JavaPlugin {
 				} catch (Exception e) {
 					log.log(Level.SEVERE, "[PartyMusic] Failed to save music database:", e);
 				}
-				sender.sendMessage(ChatColor.GREEN + args[1] + " has successfully removed!");
+				sender.sendMessage(HEADER);
+				sender.sendMessage(ChatColor.GREEN + args[1] + " has been successfully removed!");
+			}
+		} else if (args[0].equalsIgnoreCase("list")) {
+			if (!((BCE() && user.hasPerm("pmusic.list")) || sender.isOp())) {
+				sender.sendMessage(ChatColor.RED + "You don't have permission to do that!");
+			} else if (args.length != 1) {
+				sender.sendMessage(ChatColor.RED + "Proper use is: /music list");
+			} else {
+				sender.sendMessage(HEADER);
+				if (list.size() == 0) {
+					sender.sendMessage(ChatColor.RED + "There are no songs set!");
+					sender.sendMessage(ChatColor.RED + "Use /song set <name> <url> to set one!");
+				} else {
+					String output = "";
+					for(Object key : list.keySet()) {
+						if (count(output, ',') == 4) {
+							sender.sendMessage(output + ChatColor.GREEN + ((String)key));
+							output = "";
+						} else {
+							output += ChatColor.GREEN + ((String) key) + ChatColor.GRAY + ", ";
+						}
+					}
+					if (!output.isEmpty()) {
+						sender.sendMessage(output.subSequence(0, output.length() - 3).toString());
+					}
+				}
 			}
 		}
 		return true;
+	}
+	
+	public static int count(String str, char c) {
+		int count = 0;
+		for (char c2 : str.toCharArray()) {
+			if (c2 == c) {
+				count++;
+			}
+		}
+		return count;
 	}
 
 }
